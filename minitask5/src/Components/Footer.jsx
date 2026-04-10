839682a9eb04a6f7e43c78a30dd04b56326709b4
ilhammursidi <imursidi37@gmail.com>
2026-04-10 18:26:00 +0700

    feat: make the footer stay below the content

--- minitask5/src/Components/Footer.jsx
+++ minitask5/src/Components/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer(props) {
   return (
-    <footer className="bg-sky-400 h-20 w-full">
+    <footer className="bg-sky-400 p-5 w-full min-h-fit">
         <p className="pt-5 text-center"> Copyright &copy; 2020 - 2026 All Rights Reserved.</p>
-        <hr className="w-200 m-auto border-1 mt-1 " />
+        <hr className="w-200 m-auto border mt-1 " />
     </footer>
   )
 }
