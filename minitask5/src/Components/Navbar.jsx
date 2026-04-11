ac3a5df7dc0a2f4bd4b798e4915e60525b7e254a
ilhammursidi <imursidi37@gmail.com>
2026-04-11 12:47:37 +0700

    feat: add img to for user image

--- minitask5/src/Components/Navbar.jsx
+++ minitask5/src/Components/Navbar.jsx
@@ -1,33 +1,62 @@
+import { useContext } from "react";
 import { NavLink } from "react-router";
+import LoginContext from "../context/authcontext/context";
 
-function Nav(){
-    return(
+function Nav() {
+    const { user, logout, editProfile } = useContext(LoginContext)
+
+    return (
         <nav className="p-4 font-semibold">
-            <ul className="flex gap-20 justify-center">
-                <li><NavLink className={({isActive}) => isActive ? "bg-white rounded px-1" : " "}  to={"/"}>
+            <ul className="flex gap-20 justify-center items-center">
+                <li><NavLink className={({ isActive }) => isActive ? "bg-white rounded px-1" : " "} to={"/"}>
                     Home
                 </NavLink>
-                    </li>
-                <li><NavLink className={({isActive}) => isActive ? "bg-white rounded px-1" : " "} to={"/counter"}>
+                </li>
+                <li><NavLink className={({ isActive }) => isActive ? "bg-white rounded px-1" : " "} to={"/counter"}>
                     Counter
                 </NavLink>
-                    </li>
-                <li><NavLink className={({isActive}) => isActive ? "bg-white rounded px-1" : " "} to={"/product"}>
+                </li>
+                <li><NavLink className={({ isActive }) => isActive ? "bg-white rounded px-1" : " "} to={"/product"}>
                     Product
                 </NavLink>
-                    </li>
-                <li><NavLink className={({isActive}) => isActive ? "bg-white rounded px-1" : " "} to={"/pokemon"}>
+                </li>
+                <li><NavLink className={({ isActive }) => isActive ? "bg-white rounded px-1" : " "} to={"/pokemon"}>
                     Pokemon
                 </NavLink>
-                    </li>
-                <li><NavLink className={({isActive}) => isActive ? "bg-white rounded px-1" : " "} to={"/character"}>
+                </li>
+                <li><NavLink className={({ isActive }) => isActive ? "bg-white rounded px-1" : " "} to={"/character"}>
                     Rick&Morty
                 </NavLink>
-                    </li>
-                <li><NavLink className={({isActive}) => isActive ? "bg-white rounded px-1" : " "} to={"/rickmortytwo"}>
+                </li>
+                <li><NavLink className={({ isActive }) => isActive ? "bg-white rounded px-1" : " "} to={"/rickmortytwo"}>
                     Custom Hook
                 </NavLink>
-                    </li>
+                </li>
+                <li>
+                    {user ? (
+                        <section className="flex items-center gap-2">
+                            <p>{user.name}</p>
+                            <img className="w-10 rounded-full" src={user?.photo || "https://i.pravatar.cc/150?img=62"} alt="" />
+                            <select name="menu" id="menu" className="w-4">
+                                <option value=""hidden></option>
+                                <option value="Profile" onClick={editProfile}>
+                                    Profile
+                                    </option>
+                                    
+                                    
+                        <option onClick={logout} className="flex justify-center bg-emerald-400 border p-1 rounded w-20">
+                            Logout
+                        </option>
+                                
+                        
+                            </select>
+                                </section>
+                    ) : (
+                        <NavLink className="border bg-emerald-400 p-2 rounded-md" to={"/login"}>
+                            Login
+                        </NavLink>
+                    )}
+                </li>
             </ul>
         </nav>
     )
