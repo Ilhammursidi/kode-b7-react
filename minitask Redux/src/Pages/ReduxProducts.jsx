9433ae01aaf3d856eec9f0dbb6134442ab355502
ilhammursidi <imursidi37@gmail.com>
2026-04-14 00:43:03 +0700

    feat: add reduxProduct page

---
+++ minitask Redux/src/Pages/ReduxProducts.jsx
@@ -0,0 +1,11 @@
+import { FormProduct } from "../Components/FormProduct" 
+import { TabelProduct } from "../Components/TabelProduct"
+
+export function ReduxProducts() {
+    return(
+        <>
+        <FormProduct></FormProduct>
+        <TabelProduct></TabelProduct>
+        </>
+    )
+}
