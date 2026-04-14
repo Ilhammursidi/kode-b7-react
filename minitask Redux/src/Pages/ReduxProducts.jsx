f8486ba56d352574c897e25d35d55000de453e6e
ilhammursidi <imursidi37@gmail.com>
2026-04-14 16:47:00 +0700

    style: add h-screen for section

--- minitask Redux/src/Pages/ReduxProducts.jsx
+++ minitask Redux/src/Pages/ReduxProducts.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react"
 import { FormProduct } from "../Components/FormProduct" 
 import { TabelProduct } from "../Components/TabelProduct"
 
 export function ReduxProducts() {
+    const [editProduct,setEditProduct] = useState(null);
+
+    const handleEdit = (product) => {
+        setEditProduct(product);
+    }
+
+    const finishEdit = () => {
+        setEditProduct(null);
+    }
+
     return(
-        <>
-        <FormProduct></FormProduct>
-        <TabelProduct></TabelProduct>
+        < >
+        <section className="grid grid-cols-2 min-h-screen">
+        <FormProduct
+        editProduct={editProduct} onFinishEdit={finishEdit}></FormProduct>
+        <TabelProduct onEdit={handleEdit}></TabelProduct>
+        </section>
         </>
     )
 }
