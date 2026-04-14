c272239f533239875dabfb20a77a89ba99bc7de6
ilhammursidi <imursidi37@gmail.com>
2026-04-14 16:48:14 +0700

    feat: add store

--- minitask Redux/src/main.jsx
+++ minitask Redux/src/main.jsx
@@ -1,20 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Global.css'
 import { BrowserRouter } from 'react-router'
 import AppRouter from './AppRouter'
 import LoginProvider from './context/authcontext/Provider'
-import store from './redux/store'
-import { Provider } from 'react-redux'
+import { Provider as ReduxProvider } from 'react-redux'
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from './redux/store'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+      <ReduxProvider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
   <BrowserRouter>
     <LoginProvider>
-      <Provider store={store}>
     <AppRouter />
-      </Provider>
     </LoginProvider>
     </BrowserRouter>
-  </StrictMode>
+    </PersistGate>
+      </ReduxProvider>,
 )
