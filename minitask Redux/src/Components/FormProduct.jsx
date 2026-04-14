9ac4e3ba70df5f17e1839d9d3d96b29c3e82c9bd
ilhammursidi <imursidi37@gmail.com>
2026-04-14 16:43:35 +0700

    style: add border to cancel button

--- minitask Redux/src/Components/FormProduct.jsx
+++ minitask Redux/src/Components/FormProduct.jsx
@@ -1,49 +1,74 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
-import { addProduct } from "../redux/slice/product"
+import { addProduct, updateProduct } from "../redux/slice/product"
 
-export function FormProduct() {
-const [item,setItem] = useState("");
-const [qty,setQty] = useState(1);
+export function FormProduct({editProduct,onFinishEdit}) {
+const [product,setProduct] = useState("");
+const [stock,setStock] = useState(1);
+const [category,setCategory] = useState("");
 const dispatch = useDispatch();
+
+useEffect(()=> {
+    if(editProduct) {
+        setProduct(editProduct.product)
+        setStock(editProduct.stock);
+        setCategory(editProduct.category)
+    } else {
+        setProduct("");
+        setStock(1)
+        setCategory("");
+    }
+},[editProduct]);
 
 const handleSubmit = (e) => {
     e.preventDefault()
-    dispatch(addProduct({
-        product: item,
-        stock: qty,
+    if(editProduct) {
+        dispatch(updateProduct({id: editProduct.id,product,stock,category}));
+        onFinishEdit();
+    } else {
+    dispatch(addProduct({ id: Date.now(),
+        product,stock,category
     }))
-    setItem("");
-    setQty(1);
+    setProduct("");
+    setStock(1);
+    setCategory("");
 }
+};
+
     return (
-        <form onSubmit={handleSubmit} className="flex flex-col gap-5 border w-2/6 m-auto mt-10 p-5">
-            <div className="flex gap-2">
-                <label htmlFor="item"><b>Item</b></label>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5 border w-3/6 m-auto mt-10 p-5">
+            <div className="flex gap-2 items-center">
+                <label htmlFor="product"><b>Product</b></label>
                 <input
                     type="text"
-                    id="item"
-                    value={item}
-                    onChange={(e) => setItem(e.target.value)}
+                    id="product"
+                    value={product}
+                    onChange={(e) => setProduct(e.target.value)}
                     className="border p-2"
                     required
                 />
             </div>
+            <select className="text-center" name="category" id="category"
+            onChange={(e)=> setCategory(e.target.value)}>
+                <option value=""selected disabled>Pilih kategory</option>
+                <option value="makanan">Makanan</option>
+                <option value="minuman">Minuman</option>
+                <option value="rokok">Rokok</option>
+            </select>
             <div className="flex gap-2">
-                <label htmlFor="qty"><b>Quantity</b></label>
+                <label htmlFor="stock"><b>Stock</b></label>
                 <input
                     type="number"
-                    id="qty"
-                    value={qty}
-                    onChange={(e) => setQty(parseInt(e.target.value))}
+                    id="stock"
+                    value={stock}
+                    onChange={(e) => setStock(parseInt(e.target.value))}
                     className="border p-2"
                     min="1"
                     required
                 />
             </div>
             <button type="submit" className="bg-blue-500 text-white p-2 rounded">
-                Add Product
-            </button>
+                {editProduct ? "Update" : "Tambah"}</button>{editProduct && <button type="button" className="border-2 border-blue-600 text-blue-600 rounded-md p-2" onClick={onFinishEdit}>Batal</button>}
         </form>
 
     )
